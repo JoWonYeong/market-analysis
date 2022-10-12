06894389c013791914c77f23c09e0c440fcd7beb
JoWonYeong <wswy17@gmail.com>
2022-10-13 02:51:03 +0900

    2

--- src/Pages/Map.js
+++ src/Pages/Map.js
@@ -2,6 +2,7 @@ import gu from '../data/suseonggu.json'
 import dong from '../data/dong.json'
 import React, { useState, useEffect } from 'react';
 import MyModal from '../Components/MyModal'
+import insideAlgorithm from '../function/insideAlgorithm';
 const { kakao } = window;
 
 export default function Map() {
@@ -218,19 +219,5 @@ function inside(mouseEvent, map, overlay) {
         inside = insideAlgorithm(p, inside, clickX, clickY);
     }
 
-    return inside
-}
-
-// path([위도, 경도])로 이루어진 범위 안에 있으면 true, 밖에 있으면 false
-function insideAlgorithm(path, inside, clickX, clickY) {
-    for (var i = 0, j = path.length - 1; i < path.length; j = i++) {
-        var xi = path[i][0], yi = path[i][1];
-        var xj = path[j][0], yj = path[j][1];
-
-        var intersect = ((yi > clickY) != (yj > clickY)) && (clickX < (xj - xi) * (clickY - yi) / (yj - yi) + xi);
-        if (intersect) {
-            inside = !inside
-        }
-    }
     return inside
 }
